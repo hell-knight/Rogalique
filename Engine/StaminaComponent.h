02c62e652a81b8eac4ca96c5a07c20a4bb73a1ad
hell-knight <hell_knight@tutamail.com>
2026-05-04 10:09:44 -0700

    [#16] Added StaminaComponent in engine

---
+++ Engine/StaminaComponent.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Component.h"
+#include "EngineAPI.h"
+
+namespace MyEngine 
+{
+class ENGINE_API StaminaComponent : public Component{
+   public:
+    StaminaComponent(GameObject* gameObject, float maxStamina = 100.f);
+
+    void Update(float deltaTime) override {}
+    void Render() override {}
+
+    float GetStamina() const;
+    float GetMaxStamina() const;
+    void Restore(float amount);
+    void Use(float amount);
+    bool HasEnough(float amount) const;
+
+   private:
+    float stamina;
+    float maxStamina;
+};
+}
